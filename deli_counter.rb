41db8947633f19665981534511f81bccd6e27bbc
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-14 20:17:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(customer)
+   if customer.length >= 1
+    new_customers = []
+    counter = 1 
+    customer.each do |name|
+      new_customers.push("#{counter}. #{name}")
+      counter += 1
+    end  
+   puts "The line is currently: #{new_customers.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end 
+end 
+
+def take_a_number(number, name)
+  number.push(name)
+  puts "Welcome, #{name}. You are number #{number.length} in line."
+end 
+
+def now_serving(katz_deli)
+  
+end 
+
+
